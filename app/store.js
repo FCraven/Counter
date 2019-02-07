b2e47bd96ac87ae250c8971abdda0987b6aeeab6
FCraven <craven.francis@gmail.com>
2019-02-06 20:24:32 -0500

    labelled store needs

--- app/store.js
+++ app/store.js
@@ -6,6 +6,17 @@ import {createStore} from 'redux'
 //  - initial state
 //  - reducer
 
+
+//action type is a string succinctly explaining the action
+
+//action creator is a function
+
+//initial state is the same as intializing state in a component- what state do we want in the store
+
+//reducer is a switch/case that plinkos an action type through to a function that then changes state and emits the next state
+
+
+
 const initialState = {
   // what should be on our initial state?
   // add on to test remote
